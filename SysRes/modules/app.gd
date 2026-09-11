7b12ace3f8e150ff48c03fc9de2723ca046f87c0
Fishfromthemeditrean <169060445+Fishfromthemeditrean@users.noreply.github.com>
2026-09-11 13:46:03 +0300

    Delete SysRes directory not in main

--- SysRes/modules/app.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-var apps = globals.apps
-func appinit(nname):
-	print(globals.apps)
-	if nname and globals.apps:
-		var ins = nname.new()
-		globals.apps.add_child(ins)
-		ins.main()
-		print("appinit")
